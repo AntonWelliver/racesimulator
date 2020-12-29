f61b970050bbda984244308c39848692c6f233ac
Anton <antonwelliver27@gmail.com>
2020-12-29 15:04:38 +0100

    connected server and frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import RaceListScreen from './screens/RaceListScreen'
 
 const App = () => {
   return (
-    <>
-      <h1>Race Simulator</h1>
-    </>
+    <Router>
+      <Header />
+      <main>
+        <Container>
+          <Route path='/' component={RaceListScreen} exact />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ frontend/src/screens/RaceListScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RaceListScreen = () => {
+    return (
+        <>
+            <h1>Racelist</h1>
+        </>
+    )
+}
+
+export default RaceListScreen
