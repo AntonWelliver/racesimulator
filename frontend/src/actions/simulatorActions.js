8a686eebae703db5888ce63e4fedd2fda3c3b8a4
Anton <antonwelliver27@gmail.com>
2021-01-07 22:06:15 +0100

    formatting code

--- frontend/src/actions/simulatorActions.js
+++ frontend/src/actions/simulatorActions.js
@@ -1,21 +1,27 @@
-import axios from 'axios'
-import { RACE_LIST_REQUEST, RACE_LIST_SUCCESS, RACE_LIST_FAIL } from '../constants/simulatorConstants'
+import axios from 'axios';
+import {
+    RACE_LIST_REQUEST,
+    RACE_LIST_SUCCESS,
+    RACE_LIST_FAIL,
+} from '../constants/simulatorConstants';
 
 export const listRaces = () => async (dispatch) => {
     try {
-        dispatch({ type: RACE_LIST_REQUEST })
+        dispatch({ type: RACE_LIST_REQUEST });
 
-        const { data } = await axios.get(`/api/v1/race-list`)
+        const { data } = await axios.get(`/api/v1/race-list`);
 
         dispatch({
             type: RACE_LIST_SUCCESS,
-            payload: data
-        })
+            payload: data,
+        });
     } catch (error) {
         dispatch({
             type: RACE_LIST_FAIL,
             payload:
-                error.response && error.response.data.message ? error.response.data.message : error.message
-        })
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
     }
-}
+};
